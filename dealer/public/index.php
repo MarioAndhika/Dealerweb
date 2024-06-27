4adf15d67f75a933e29a59378a957305e70dc55f
Habibur Rohman <habiburrohman@students.amikom.ac.id>
2024-06-27 18:41:10 +0700

    lanjutan

---
+++ dealer/public/index.php
@@ -0,0 +1,199 @@
+<?php
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda | Auto</title>
+    <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('resources/css/bootstrap-grid.min.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body style="background-color: #1A1A1A;">
+    <!-- header -->
+    <header>
+        <nav class="navbar navbar-expand">
+            <div class="container"> 
+                <a href="#" class="navbar-brand"> 
+                    <img class="logo" src="img/logo_dealer.png" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.html">Beranda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="tentangkami.html">Tentang Kami</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Jenis Mobil
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="suv.html">SUV</a></li>
+                                <li><a class="dropdown-item" href="sedan.html">SEDAN</a></li>
+                                <li><a class="dropdown-item" href="mpv.html">MPV</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="tim.html">Tim</a>
+                        </li>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="kontak.html">Kontak</a>
+                        </li>
+                    </ul>
+                </div>
+            
+            </div>
+        </nav>
+    </header>
+
+    <!-- slider -->
+    <div class="slide">
+        <img class="myslide" src="img/silde 2.png" alt="Gambar 1">
+        <img class="myslide" src="img/hasil mobil.png" alt="Gambar 2">
+    <!-- tambahkan slide lainnya jika diperlukan -->
+    </div> 
+
+    <!-- model teratas -->
+    <div class="model">
+        <h3>Model Teratas</h3>
+        <ul>
+            <li>
+                <img src="img/c3.jpeg" alt="Mobil 1">
+                <p>Citroen C3
+                    <br>
+                    Rp225 - 236 Juta</p>
+            </li>
+            <li>
+                <img src="img/MG-5-GT.jpg" alt="Mobil 1">
+                <p>Mg 5 GT
+                    <br>
+                    Rp347.9 - 407.9 Juta</p>
+            </li>
+            <li>
+                <img src="img/bmw 320i.jpeg" alt="Mobil 1">
+                <p>Bmw 320i
+                    <br>
+                    Rp837 Jt - 1.145 Milyar</p>
+            </li>
+            <li>
+                <img src="img/mercedes.jpg" alt="Mobil 1">
+                <p>Mercedes-Benz AMG GT R Coupe
+                    <br>
+                    Rp837 Jt - 1.145 Milyar</p>
+            </li>
+        </ul>
+    </div>
+
+    <!-- model pilihan -->
+    <div class="model">
+        <h3>Model Pilihan</h3>
+        <ul>
+            <li>
+                <img src="img/Toyota Alp.jpeg" alt="Mobil 1">
+                <p>Toyota Alphard
+                    <br>
+                    Rp1.381 - 1.682 Milyar</p>
+            </li>
+            <li>
+                <img src="img/Honda HR-V.jpeg" alt="Mobil 1">
+                <p>Honda HR-V
+                    <br>
+                    Rp383.9 - 540.3 Juta</p>
+            </li>
+            <li>
+                <img src="img/Chery Omoda 5.jpeg" alt="Mobil 1">
+                <p>Chery Tiggo 8 Pro
+                    <br>
+                    Rp528.5 - 558.5 Juta</p>
+            </li>
+            
+        </ul>
+    </div>
+
+    <!-- model terbaru -->
+    <div class="model">
+        <h3>Model Terbaru</h3>
+        <ul>
+            <li>
+                <img src="img/livina.jpg" alt="Mobil 1">
+                <p>Nissan Livina
+                    <br>
+                    Rp292.5 - 327 Juta</p>
+            </li>
+            <li>
+                <img src="img/slide2.jpg" alt="Mobil 1">
+                <p>Lexus LS 500 Executive
+                    <br>
+                    Rp3.745 Milyar</p>
+            </li>
+            <li>
+                <img src="img/wuling Almaz.jpeg" alt="Mobil 1">
+                <p>Wuling Almaz Hybrid
+                    <br>
+                    Rp476 Juta</p>
+            </li>
+            <li>
+                <img src="img/Mazda CX-30.jpg" alt="Mobil 1">
+                <p>Mazda CX-30
+                    <br>
+                    Rp572.2 Juta</p>
+            </li>
+        </ul>
+    </div>
+
+    <!-- footer -->
+    <footer class="footer" style="background-color: #333; color: white; padding: 50px 0;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h3>Dealer Mobil</h3>
+                    <p>Dealer Mobil adalah perusahaan yang bergerak di bidang penjualan mobil. Dealer Mobil menyediakan berbagai jenis mobil sesuai kebutuhan Anda.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Alamat</h3>
+                    <p>Jl. Raya Kediri, No. 1, Kediri, Jawa Timur</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>Kontak</h3>
+                    <p>Telepon: 08123456789</p>
+                    <p>Email: info@dealermobil.com</p>
+                    
+                </div>
+            </div>
+            <p>&copy; 2024 AutoDeal. All rights reserved.</p>
+        </div>
+    </footer>
+
+                    
+</body>
+
+<!-- script -->
+    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script>
+        var myIndex = 0;
+        carousel();
+        
+        function carousel() {
+          var i;
+          var x = document.getElementsByClassName("myslide");
+          for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+          }
+          myIndex++;
+          if (myIndex > x.length) {myIndex = 1}    
+          x[myIndex-1].style.display = "block";  
+          setTimeout(carousel, 3000); // Change image every 3 seconds
+        }
+        </script>
+</html>
